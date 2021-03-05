de87a41ab6ff450e262618abf84ace358460944c
Floria Ermelinda <fermelin@il-k5.msk.21-school.ru>
2021-03-05 11:21:03 +0300

    add test file:

---
+++ tests/gettimeofday.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gettimeofday.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fermelin <fermelin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/05 10:50:10 by fermelin          #+#    #+#             */
+/*   Updated: 2021/03/05 11:14:30 by fermelin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <sys/time.h>
+
+int	main(void)
+{
+	int retval;
+	long int sec;
+	long int  mins;
+	long int  hrs;
+	long int  days;
+	long int  yrs;
+	struct timeval tp;
+
+	retval = 0;
+	retval = gettimeofday(&tp, NULL);
+	sec = tp.tv_sec;
+	mins = sec / 60;
+	hrs = mins / 60;
+	days = hrs / 24;
+	yrs = days / 365;
+	printf("sec %ld\n", sec);
+	printf("mins %ld\n", mins);
+	printf("hrs %ld\n", hrs);
+	printf("days %ld\n", days);
+	printf("yrs %ld\n", yrs);
+	return (0);
+}
